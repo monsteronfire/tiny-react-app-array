3628844a43527df168fd0f153f1df648f6990a3f
Shaii Ong <monsteronfire@gmail.com>
2017-06-27 18:03:01 +0800

    Add ItemList component

--- app/components/App.js
+++ app/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import ItemList from './ItemList';
 
 class App extends React.Component {
   render() {
     return (
       <div className='container'>
-        <p>Hello</p>
+        <ItemList/>
       </div>
     )
   }
---
+++ app/components/ItemList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ItemList extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Item List</p>
+      </div>
+    )
+  }
+}
+
+module.exports = ItemList;
